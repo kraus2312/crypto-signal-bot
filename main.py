95c8853ccc1d3c2be7ac1a116f8f8a32e7005f8e
kraus2312 <igor231294horokhov@gmail.com>
2025-06-22 00:03:25 +0300

    fix: handle empty PORT env variable

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ async def on_shutdown(dp):
 
 if __name__ == "__main__":
     from aiogram import executor
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 5000)),
-    )
+  executor.start_webhook(
+    dispatcher=dp,
+    webhook_path=WEBHOOK_PATH,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    host="0.0.0.0",
+    port=int(os.getenv("PORT") or 5000),  # ← Ось так
+)
